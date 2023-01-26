55580f8ff66904c43d014138699c18c6d34ca929
Jaredzu <jared.zu96@gmail.com>
2023-01-26 14:55:04 -0500

    feat: update navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -4,8 +4,38 @@ import { Outlet } from 'react-router'
 const Navbar = () => {
     return (
         <>
-            <div>Navbar</div>
-            <Outlet />
+            <nav className="navbar navbar-expand-sm bg-light">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/" >
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/admin" >
+                                Admin
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/users" >
+                                Users
+                            </Link>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+            <div className="container p-2">
+                <Outlet />
+
+            </div>
+
         </>
     )
 }
