e8b67782a49400551796d85a81f7a8d9494edf88
Jaredzu <jared.zu96@gmail.com>
2023-01-26 14:51:44 -0500

    feat: added navbar

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,18 @@
 
 import React from "react"
 import { Home, Users } from "./pages"
+import Navbar from "./layout/Navbar"
 import { createBrowserRouter, Route, RouterProvider, createRoutesFromElements } from "react-router-dom"
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <>
-            <Route path="/" element={<Home />} />
-            <Route path="/users" element={<Users />} />
+            <Route path="/" element={<Navbar />}>
+
+                <Route path="/" element={<Home />} />
+                <Route path="/users" element={<Users />} />
+
+            </Route>
         </>
     )
 )
