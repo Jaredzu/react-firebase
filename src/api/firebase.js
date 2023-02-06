deb1cc12c3d63f9d78b7e0b14d9ac20e68ce2133
Jaredzu <jared.zu96@gmail.com>
2023-02-05 21:33:15 -0500

    feat: added firebase

---
+++ src/api/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD-pKsjCXAWmVoeC5Vx82wUNUSTbDF8Ack",
+  authDomain: "deploys-bootcamp-jared.firebaseapp.com",
+  projectId: "deploys-bootcamp-jared",
+  storageBucket: "deploys-bootcamp-jared.appspot.com",
+  messagingSenderId: "322212227952",
+  appId: "1:322212227952:web:bc172594a551ff224cbdb5"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
